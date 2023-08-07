0ef27d657c4e074f8d1342088380462585dd71a8
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-07 19:27:52 +0300

    Update App.js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,7 +1,7 @@
 import { RegistrationScreen } from "./src/screens/RegistrationScreen";
 import { LoginScreen } from "./src/screens/LoginScreen";
 import { PostsScreen } from "./src/screens/PostsScreen";
-import {ProfileScreen} from "./src/Screens/ProfileScreen"
+import { ProfileScreen } from "./src/Screens/ProfileScreen"
 import { useFonts } from 'expo-font';
 import { useCallback } from 'react';
 
@@ -13,16 +13,38 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     'Roboto-Medium': require('./src/assets/fonts/Roboto-Medium.ttf'),
     'Roboto-Regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
+    'RobotoMedium': require("./screens/fonts/Roboto-Medium.ttf"),
   });
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
+  const registerInfo=(info)=>{
+    console.debug(info);
   }
-  return <ProfileScreen />;
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <RegistrationScreen registerInfo={registerInfo}/>
+      <LoginScreen logininfo={loginInfo}/>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop:20,
+  },
+});
+  
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+  // return <ProfileScreen />;
