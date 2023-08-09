974966fcd311667f0a3e8dec79c65c62da1d5582
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-09 11:01:15 +0300

    Create Background.jsx

---
+++ AwesomeProject/src/components/Background.jsx
@@ -0,0 +1,19 @@
+import { View, Image } from "react-native";
+import photoBg from "../../src/screens/PhotoBG.jpg";
+
+function Background() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        position: "absolute",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <Image source={photoBg} style={{ width: "100%" }} resizeMode="cover" />
+    </View>
+  );
+}
+
+export default Background;
