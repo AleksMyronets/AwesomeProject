f526b16d6cf8eef617636825adb966f3fc1dfd6b
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-15 14:29:46 +0300

    Update RegistrationScreen.jsx

--- AwesomeProject/src/screens/RegistrationScreen.jsx
+++ AwesomeProject/src/screens/RegistrationScreen.jsx
@@ -188,7 +188,7 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     width: 25,
     height: 25,
-    left: Platform.OS === "ios" ? "67%" : "69%",
+    left: Platform.OS === "android" ? "67%" : "69%",
     top: 20,
     flex: 1,
     alignItems: "center",
